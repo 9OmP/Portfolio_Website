83a1072cac2b6a327202f2cf6867dd35a0a9a819
9OmP <ompatil16022002@gmail.com>
2024-05-11 08:15:16 +0530

    completed code

--- script.js
+++ script.js
@@ -62,3 +62,66 @@ circles.forEach(elem=>{
 
 //------------------mix it up portfolio section-------------------------------------- /////
 var mixer = mixitup('.portfolio-gallery');
+
+
+
+
+//-------------------Active Menu---------------------------------------------------------
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
+
+
+//-------------------Sticky Navbar---------------------------------------------------------
+const header = document.querySelector("header");
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 50)
+})
+
+//------------------- Parallax ---------------------------------------------------------
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));        
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
+
+
+//-------------------Toggle Icon Navbar---------------------------------------------------------
+let menuIcon = document.querySelector("#menu-item");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
+
+
