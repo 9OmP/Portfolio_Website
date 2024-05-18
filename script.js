f521f04aa198f0c0667a935514b4cf9aa6e6a783
Om <104563841+9OmP@users.noreply.github.com>
2024-05-19 01:09:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ let changeText = () => {
     currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
 };
 
+setInterval(changeText, 3000);
 changeText();
 setInterval(changeText, 3000);
 
